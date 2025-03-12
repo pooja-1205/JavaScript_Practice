5126643a0c5fccc99449b982abbc67014bf31de1
pooja-1205 <nandguruwarpooja@gmail.com>
2025-03-12 23:43:52 +0530

    Done Changes

---
+++ 01_variable.js
@@ -0,0 +1,24 @@
+console.log("Welcome to JS");
+// const declaration
+const acountId = 144553;
+//variable declaration
+let acountEmail = "pooja@gmail.com";
+var acountPassword = "12344";
+acountCity = "Nagpur";
+let acountState; //value is undefined
+
+// acountId = 2; // not allowed
+acountEmail = "pn@pc.cpm";
+acountPassword = "1545255";
+acountCity = "Pune";
+
+console.log(acountId);
+
+console.table([acountId, acountEmail, acountPassword, acountCity, acountState]);
+
+// { } - called scope
+
+/*
+Perefer not to use var 
+because of issue in block scope and functional scope
+*/
