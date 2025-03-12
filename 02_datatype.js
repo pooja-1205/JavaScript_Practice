8c1e11b1f635a9f261ea7dc2312bb9cc3e0a1acf
pooja-1205 <nandguruwarpooja@gmail.com>
2025-03-13 00:07:22 +0530

    Done Changes

--- 02_datatype.js
+++ 02_datatype.js
@@ -6,3 +6,22 @@ console.log(3 + 3);
 console.log("Pooja"); //code readability not good
 console.log(3 + 3);
 console.log("Pooja");
+
+let name = "sakshi";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => range of 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone vale
+// undefined => value is not define
+// symbol => unique
+// object
+
+console.log(typeof "pooja");
+console.log(typeof age);
+console.log(typeof null); //type is object
+console.log(typeof undefined);
