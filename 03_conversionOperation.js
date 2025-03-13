237f80f0e8384fb8833dd78112e5aa857e3a058e
pooja-1205 <nandguruwarpooja@gmail.com>
2025-03-13 19:08:52 +0530

    Done Changes

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,3 +1,5 @@
+/*******conversion************/
+
 let score = 33;
 let scope = "33abd"; //its also be type number
 console.log(typeof score);
@@ -39,3 +41,36 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 // 33 => 33 string
+
+/**********Operations***********/
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " pooja";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(((3 + 4) * 5) % 3);
+console.log(+true); //1 wrong true+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // wrong idea issue with readability
+
+let gameCounter = 100;
+gameCounter++; //incremental oprator
+console.log(gameCounter);
