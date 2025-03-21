4cf64ca7eec9d6b4606dcce710035b219a6d16ee
pooja-1205 <nandguruwarpooja@gmail.com>
2025-03-21 19:06:49 +0530

    Done Changes

--- 02_Arrays/01_arrays.js
+++ 02_Arrays/01_arrays.js
@@ -18,20 +18,20 @@ const myArr2 = new Array(1, 2, 3, 4);
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join() // bind aaray and convert it int string type o/p => 0,1,2,3,4,5
 
 // console.log(myArr);
 // console.log( newArr);
 
 // slice, splice diffrence most imp
 
-console.log("A ", myArr);
+console.log("A ", myArr); //o/p => [0,1,2,3,4,5]
 
-const myn1 = myArr.slice(1, 3);
+const myn1 = myArr.slice(1, 3); //return section of array
 
-console.log(myn1);
-console.log("B ", myArr);
+console.log(myn1); // o/p => [1, 2]
+console.log("B ", myArr); // o/p => [0,1,2,3,4,5]
 
 const myn2 = myArr.splice(1, 3);
-console.log("C ", myArr);
-console.log(myn2);
+console.log("C ", myArr); // o/p => [0, 4, 5] orignal array mese splice array removed he
+console.log(myn2); // o/p => [1, 2, 3]
